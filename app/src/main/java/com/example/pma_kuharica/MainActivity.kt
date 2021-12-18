a15b776caba8ac869867bb6b4fa60ae7ad695289
Roko2 <rokokovac77@gmail.com>
2021-12-18 22:30:36 +0100

    Početni activity za login i main, firebase google auth prijava i odjava

--- app/src/main/java/com/example/pma_kuharica/MainActivity.kt
+++ app/src/main/java/com/example/pma_kuharica/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.pma_kuharica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.NonNull
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.SignInButton
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gumbOdjava=findViewById<Button>(R.id.btnOdjava)
+        gumbOdjava.setOnClickListener{signOut()}
     }
+    private fun signOut() {
+        Firebase.auth.signOut()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
 }
